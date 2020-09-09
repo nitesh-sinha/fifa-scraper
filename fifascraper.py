79a8bb6a0ae0e640724a88fe190ab1814efd8a89
Nitesh Sinha <mit.nitesh@gmail.com>
2020-09-09 15:49:22 +0530

    Moved creation of all links to main, scraper now just works on one HTML page at a time, Updated names of scraper methods

--- fifascraper.py
+++ fifascraper.py
@@ -4,7 +4,8 @@ import time
 import numpy as np
 # TODO: Add tqdm
 
-def scrape_player_image(html, player_img_links):
+# Scrapes player images(almost 60) on one HTML page
+def scrape_player_images(html, player_img_links):
     # Scrape player images
     player_image_boxes = html.findAll("td", {"class": "col-avatar"})
     # print("Number of player images on this page = {}".format(len(player_image_boxes)))
@@ -15,19 +16,20 @@ def scrape_player_image(html, player_img_links):
             player_img_links.append(player_image.figure.img['data-src'])
 
 
-def scrape_player_id(html, player_ids):
+# Scrapes player ids(almost 60) on one HTML page
+def scrape_player_ids(html, player_ids):
     # Scrape player ids
     player_id_boxes = html.findAll("a", {"class": "tooltip"})
-    num_players_on_page = min(60, len(player_id_boxes))  # Eliminate 4 extra unwanted boxes towards end of page
-    for idx in range(0, num_players_on_page):
+    for player_id_box in player_id_boxes[:-4]:   # Eliminate 4 extra unwanted boxes towards end of page
         # href = /player/252886/kwame-osigwe/200056/
         # is in the form /player/ID/....
         # Extract the ID
-        href = player_id_boxes[idx]['href']
+        href = player_id_box['href']
         player_ids.append(href.split("/")[2])
 
 
-def scrape_player_name_country_club(html, player_names, country_img_links, club_img_links):
+# Scrapes player names, country and club logo links(almost 60) on one HTML page
+def scrape_player_name_country_clubs(html, player_names, country_img_links, club_img_links):
     # Scrape player name along with his country and club images
     country_club_image_boxes = html.findAll("div", {"class": "bp3-text-overflow-ellipsis"})
     # print("Number of countries+club images on this page = {}".format(len(country_club_image_boxes)))
@@ -53,7 +55,8 @@ def scrape_player_name_country_club(html, player_names, country_img_links, club_
             continue
 
 
-def go_scrape():
+# Scrapes the page at the given url(with 60 players on it)
+def go_scrape(url):
     # Without user-agent header, this site return 403 Forbidden
     user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.37"
     scraping_started = False
@@ -62,36 +65,26 @@ def go_scrape():
     player_names = []
     country_img_links = []
     club_img_links = []
-    for offset in range(0, 120, 60):
-        try:
-            #time.sleep(1)
-            print("Processing page at offset " + str(offset))
-            sofifa_url = "https://sofifa.com/players?offset=" + str(offset) # preparing the URL
-            player_list_page = requests.get(sofifa_url, headers={'User-Agent': user_agent}) # requesting the webpage from the internet
-            player_list_html = bs(player_list_page.text, "html.parser") # parsing the webpage as HTML
+    try:
+        player_list_page = requests.get(url, headers={'User-Agent': user_agent}) # requesting webpage from the internet
+        player_list_html = bs(player_list_page.text, "html.parser") # parsing the webpage as HTML
 
-            # Check if all players have been scraped already
-            # Use "Previous" button as a deciding factor
-            # Noticed that some intermediate pages sometimes miss "next" button
-            # TODO: update logic since "Previous" might not always be second from last in button list
-            # buttons = player_list_html.findAll("span", {"class": "bp3-button-text"})
-            # # After starting to scrape, keep scraping only until Previous button is visible on current page
-            # if scraping_started and buttons[-2].text != "Previous":
-            #     break
-            # scraping_started = True # This should be True until scraping continues
+        # Check if all players have been scraped already
+        # Use "Previous" button as a deciding factor
+        # Noticed that some intermediate pages sometimes miss "next" button
+        # TODO: update logic since "Previous" might not always be second from last in button list
+        # buttons = player_list_html.findAll("span", {"class": "bp3-button-text"})
+        # # After starting to scrape, keep scraping only until Previous button is visible on current page
+        # if scraping_started and buttons[-2].text != "Previous":
+        #     break
+        # scraping_started = True # This should be True until scraping continues
 
-            scrape_player_image(player_list_html, player_img_links)
-            scrape_player_id(player_list_html, player_ids)
-            scrape_player_name_country_club(player_list_html, player_names, country_img_links, club_img_links)
-            # print("Number of country images on this page = {}".format(len(country_img_links)))
-            # print("Number of players successfully processed on this page = {}".format(len(player_img_links)))
-        except Exception as e:
-            print("Exception occured: " + str(e))
-            continue
+        scrape_player_images(player_list_html, player_img_links)
+        scrape_player_ids(player_list_html, player_ids)
+        scrape_player_name_country_clubs(player_list_html, player_names, country_img_links, club_img_links)
+        # print("Number of country images on this page = {}".format(len(country_img_links)))
+        # print("Number of players successfully processed on this page = {}".format(len(player_img_links)))
+    except Exception as e:
+        print("Exception occured: " + str(e))
 
-    for (p_id, p_name, p_link, ctry_link, clb_link) in zip(player_ids, player_names, player_img_links, country_img_links, club_img_links):
-        print(p_id, p_name, p_link, ctry_link, clb_link, sep="        ")
-
-    print("Finally we have {} links of players, {} links of countries and {} links of clubs".format(len(player_img_links), len(country_img_links), len(club_img_links)))
     return player_ids, player_names, player_img_links, country_img_links, club_img_links
-
--- single.py
+++ single.py
@@ -7,18 +7,44 @@ def create_dataframe(ids, names, p_links, c_links, cl_links):
         dict = {'ID':ids, 'Name': names, 'Photo':p_links, 'Flag':c_links, 'Club Logo':cl_links}
         df = pd.DataFrame(dict)
         df.to_csv("working-links.csv")
+        return df
     except Exception as e:
         print("Exception creating or storing the dataframe: " + str(e))
 
 
 if __name__ == "__main__":
+    player_ids = []
+    player_names = []
+    player_img_links = []
+    country_img_links = []
+    club_img_links = []
     print("Starting to scrape!!!")
-    (ids, names, p_links, c_links, cl_links) = go_scrape()
-    create_dataframe(ids, names, p_links, c_links, cl_links)
+    sofifa_urls = ["https://sofifa.com/players?offset=" + str(offset) for offset in range(420, 600, 60)]
+    for url in sofifa_urls:
+        print("Processing page at " + url)
+        try:
+            (ids, names, p_links, c_links, cl_links) = go_scrape(url)
+            player_ids.extend(ids)
+            player_names.extend(names)
+            player_img_links.extend(p_links)
+            country_img_links.extend(c_links)
+            club_img_links.extend(cl_links)
+        except Exception as e:
+            print("Exception during scrape: " + str(e))
+            continue
 
+    # for (p_id, p_name, p_link, ctry_link, clb_link) in zip(player_ids, player_names, player_img_links,
+    #                                                        country_img_links, club_img_links):
+    #     print(p_id, p_name, p_link, ctry_link, clb_link, sep="        ")
+    print("Finally we have {} IDs, {} player name, {} links of player-images, {} links of countries and {} "
+          "links of clubs".format(len(player_ids), len(player_names), len(player_img_links),
+                                  len(country_img_links), len(club_img_links)))
 
-
-
-
-
-
+    try:
+        # All pages scraped successfully. Now create a dataframe with scraped data
+        df = create_dataframe(player_ids, player_names, player_img_links,
+                         country_img_links, club_img_links)
+        print("Dataframe created successfully")
+        print(df)
+    except Exception as e:
+        print("Exception during dataframe creation: " + str(e))
