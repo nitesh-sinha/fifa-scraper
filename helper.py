18e39c84f8b1954782b5cf0d82b70de0348bf038
Nitesh Sinha <mit.nitesh@gmail.com>
2020-09-10 12:00:28 +0530

    Enhanced scraper to add multithreading capability for much faster scraping

---
+++ helper.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def display_scraped_data(ids, names, p_links, c_links, cl_links):
+    # for (p_id, p_name, p_link, ctry_link, clb_link) in zip(ids, names, p_links,
+    #                                                        c_links, cl_links):
+    #     print(p_id, p_name, p_link, ctry_link, clb_link, sep="        ")
+    print("Finally we have {} IDs, {} player name, {} links of player-images, {} links of countries and {} "
+          "links of clubs".format(len(ids), len(names), len(p_links),
+                                  len(c_links), len(cl_links)))
+
+
+def create_dataframe(ids, names, p_links, c_links, cl_links):
+    try:
+        dict = {'ID':ids, 'Name': names, 'Photo':p_links, 'Flag':c_links, 'Club Logo':cl_links}
+        df = pd.DataFrame(dict)
+        return df
+    except Exception as e:
+        print("Exception creating or storing the dataframe: " + str(e))
--- multithreaded.py
+++ multithreaded.py
@@ -1,10 +1,19 @@
-# Single threaded implementation of FIFA website scraper
+# Multi-threaded implementation of FIFA website scraper
+
 from fifascraper import go_scrape
 from helper import *
+import concurrent.futures
 from time import time
 
 
+def worker_scraper(url):
+    print("Processing page at " + url)
+    return go_scrape(url)
+
+
 def main():
+    # After running some experiments, this seems to be the best value for maximum efficiency
+    NUMBER_THREADS = 20
     player_ids = []
     player_names = []
     player_img_links = []
@@ -12,18 +21,17 @@ def main():
     club_img_links = []
     print("Starting to scrape!!!")
     sofifa_urls = ["https://sofifa.com/players?offset=" + str(offset) for offset in range(0, 20000, 60)]
-    for url in sofifa_urls:
-        print("Processing page at " + url)
-        try:
-            (ids, names, p_links, c_links, cl_links) = go_scrape(url)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NUMBER_THREADS) as executor:
+        # submit() returns a Future instance, which encapsulates the asynchronous execution of a callable
+        futures = [executor.submit(worker_scraper, url) for url in sofifa_urls]
+        for future in futures:
+            (ids, names, p_links, c_links, cl_links) = future.result() # blocks until a result is returned
             player_ids.extend(ids)
             player_names.extend(names)
             player_img_links.extend(p_links)
             country_img_links.extend(c_links)
             club_img_links.extend(cl_links)
-        except Exception as e:
-            print("Exception during scrape: " + str(e))
-            continue
 
     display_scraped_data(player_ids, player_names, player_img_links, country_img_links, club_img_links)
 
@@ -38,7 +46,7 @@ def main():
         print("Exception during dataframe creation: " + str(e))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     ts = time()
     main()
     print("Took {} secs to complete".format(time() - ts))
